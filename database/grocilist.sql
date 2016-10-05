a48412af97f8d8a12ce758f86fc5cc1e359b6b8c
chathura-fernando <chathurasachithra@gmail.com>
2016-10-06 01:18:08 +0530

    DB dump

--- database/grocilist.sql
+++ database/grocilist.sql
@@ -16,6 +16,58 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `trn_invitations`
+--
+
+DROP TABLE IF EXISTS `trn_invitations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trn_invitations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `status` tinyint(3) DEFAULT NULL COMMENT '1 New\n2 Send\n3 Active\n4 Expire',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trn_invitations`
+--
+
+LOCK TABLES `trn_invitations` WRITE;
+/*!40000 ALTER TABLE `trn_invitations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trn_invitations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trn_invite_friends`
+--
+
+DROP TABLE IF EXISTS `trn_invite_friends`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trn_invite_friends` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `invitation_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trn_invite_friends`
+--
+
+LOCK TABLES `trn_invite_friends` WRITE;
+/*!40000 ALTER TABLE `trn_invite_friends` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trn_invite_friends` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `trn_items`
 --
 
@@ -43,6 +95,116 @@ LOCK TABLES `trn_items` WRITE;
 /*!40000 ALTER TABLE `trn_items` DISABLE KEYS */;
 /*!40000 ALTER TABLE `trn_items` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `trn_order`
+--
+
+DROP TABLE IF EXISTS `trn_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trn_order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `total` double DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `time_slot` varchar(255) DEFAULT NULL,
+  `instructions` varchar(255) DEFAULT NULL,
+  `status` tinyint(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trn_order`
+--
+
+LOCK TABLES `trn_order` WRITE;
+/*!40000 ALTER TABLE `trn_order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trn_order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trn_order_details`
+--
+
+DROP TABLE IF EXISTS `trn_order_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trn_order_details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) DEFAULT NULL,
+  `item_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `unit_price` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trn_order_details`
+--
+
+LOCK TABLES `trn_order_details` WRITE;
+/*!40000 ALTER TABLE `trn_order_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trn_order_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trn_request_products`
+--
+
+DROP TABLE IF EXISTS `trn_request_products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trn_request_products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `contact_no` varchar(255) DEFAULT NULL,
+  `details` varchar(1000) DEFAULT NULL,
+  `status` tinyint(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trn_request_products`
+--
+
+LOCK TABLES `trn_request_products` WRITE;
+/*!40000 ALTER TABLE `trn_request_products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trn_request_products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trn_user`
+--
+
+DROP TABLE IF EXISTS `trn_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trn_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `invitation_id` int(11) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `status` tinyint(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trn_user`
+--
+
+LOCK TABLES `trn_user` WRITE;
+/*!40000 ALTER TABLE `trn_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trn_user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -53,4 +215,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-10-05 23:28:19
+-- Dump completed on 2016-10-06  1:17:26
